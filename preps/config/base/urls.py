3a47a42224d74ce525b1c3612328bf50c1a26e24
Jeremy Bowers <bowersj@washpost.com>
2011-06-17 18:51:08 -0400

    Solve static media?

--- preps/config/base/urls.py
+++ preps/config/base/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
+from preps.config.dev.settings import MEDIA_ROOT
 
 urlpatterns = patterns('',
     url(r'^sports/high-schools/', include('preps.apps.urls')),
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': preps.config.dev.settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': MEDIA_ROOT}),
 )
