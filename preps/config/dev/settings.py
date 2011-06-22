a3502c1ac1fbe4893d362ccc50b5d86563727d6e
Jeremy Bowers <bowersj@washpost.com>
2011-06-21 21:28:51 -0400

    Updated settings with installed apps.

--- preps/config/dev/settings.py
+++ preps/config/dev/settings.py
@@ -2,3 +2,24 @@ try:
     from preps.config.dev.local_settings import *
 except ImportError:
     pass
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    # 'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'preps.apps.sports',
+    'preps.apps.sports.football',
+    'preps.apps.sports.volleyball',
+    'preps.apps.sports.girls_soccer',
+    'preps.apps.sports.boys_soccer',
+    'preps.apps.sports.softball',
+    'preps.apps.sports.baseball',
+    'preps.apps.sports.girls_lacrosse',
+    'preps.apps.sports.boys_lacrosse',
+    'preps.apps.sports.girls_basketball',
+    'preps.apps.sports.boys_basketball',
+)
