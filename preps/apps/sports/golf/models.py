188afd3f51d02ee96633c10b04e9a6a8ed4b4814
Jeremy Bowers <jeremyjbowers@gmail.com>
2011-06-24 09:52:02 -0400

    Added models for meet sports.

--- preps/apps/sports/golf/models.py
+++ preps/apps/sports/golf/models.py
@@ -2,9 +2,20 @@ import datetime
 from django.db import models
 from django.template.defaultfilters import slugify
 from preps.apps.models import ModelBase
-from preps.apps.sports.models import Season, School, Sport, Conference, MeetBase
+from preps.apps.sports.models import Season, School, GameBase, Sport, Player, Conference
 from preps.apps.utils import functions as preps_utils
 
 class Meet(MeetBase):
-    teams = models.ManyToManyField(School)
+    season                          = models.ForeignKey(Season, related_name='golf_meet_season')
+	teams 							= models.ManyToManyField(School, null=True)
+	
+	def __unicode__(self):
+		if self.meet_date_and_time == None or self.meet_date_and_time == '':
+        	return u'Meet: %s golf (%s)' % (self.get_status_display(), self.id)
+		else:
+			return u'Meet [%s]: %s golf (%s)' % (self.meet_date_and_time, self.get_status_display(), self.id) 
     
+    def save(self, *args, **kwargs):
+        if self.slug == None or self.slug == '':
+            self.slug               = slugify(self.__unicode__())
+        super(Meet, self).save(*args, **kwargs)
